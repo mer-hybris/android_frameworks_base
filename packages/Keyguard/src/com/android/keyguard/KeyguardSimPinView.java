93e7642d28a815cf0af650572c85ba1824335500
Dirk Vogt <dirk@fairphone.com>
2017-02-28 10:39:43 +0100

    Revert "SIM PIN Retry attempts display on Bootup."
    This reverts commit 2c68d4432bc2a5bee148ed9a26c88541dea4dd58.
    FPIIM-258
    
    Change-Id: I8692f43162c3da936c347b56fb111a00d09e0021

--- packages/Keyguard/src/com/android/keyguard/KeyguardSimPinView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSimPinView.java
@@ -60,7 +60,12 @@ public class KeyguardSimPinView extends KeyguardPinBasedInputView {
         @Override
         public void onSimStateChanged(int subId, int slotId, State simState) {
            if (DEBUG) Log.v(TAG, "onSimStateChanged(subId=" + subId + ",state=" + simState + ")");
-           resetState();
+           switch (simState) {
+               case NOT_READY:
+               case ABSENT:
+                   closeKeyGuard();
+                   break;
+           }
        };
     };
 
@@ -257,12 +262,7 @@ public class KeyguardSimPinView extends KeyguardPinBasedInputView {
                             }
                             resetPasswordText(true /* animate */);
                             if (result == PhoneConstants.PIN_RESULT_SUCCESS) {
-                                KeyguardUpdateMonitor.getInstance(getContext())
-                                        .reportSimUnlocked(mSubId);
-                                mRemainingAttempts = -1;
-                                if (mCallback != null) {
-                                    mCallback.dismiss(true);
-                                }
+                                closeKeyGuard();
                             } else {
                                 mShowDefaultMessage = false;
                                 if (result == PhoneConstants.PIN_PASSWORD_INCORRECT) {
@@ -303,6 +303,16 @@ public class KeyguardSimPinView extends KeyguardPinBasedInputView {
     @Override
     public boolean startDisappearAnimation(Runnable finishRunnable) {
         return false;
+    }
+
+    private void closeKeyGuard() {
+        if (DEBUG) Log.d(TAG, "closeKeyGuard: Verification Completed, closing Keyguard.");
+        mRemainingAttempts = -1;
+        KeyguardUpdateMonitor.getInstance(getContext())
+                         .reportSimUnlocked(mSubId);
+        mCallback.dismiss(true);
+        mShowDefaultMessage = true;
+        reset();
     }
 
     private void showDefaultMessage() {
