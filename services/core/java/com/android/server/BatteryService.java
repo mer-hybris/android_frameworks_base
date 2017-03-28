81447bb048aed20c3f96aeff219a7752afecce3f
Borjan Tchakaloff <borjan@fairphone.com>
2017-03-28 23:39:33 +0200

    Turn the charging LED to green only when battery is full
    The charging LED was lit green when the charging battery reached
    a specific threshold. Because of our own lock-screen battery meter
    (see FPIIM-202), we want to align the green color to *fully* charged
    only.
    
    FPIIM-290
    
    Change-Id: Id14566313ca0bcb04a71f1e74a40c467d6b1ed08

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -794,8 +794,8 @@ public final class BatteryService extends SystemService {
                 }
             } else if (status == BatteryManager.BATTERY_STATUS_CHARGING
                     || status == BatteryManager.BATTERY_STATUS_FULL) {
-                if (status == BatteryManager.BATTERY_STATUS_FULL || level >= 90) {
-                    // Solid green when full or charging and nearly full
+                if (status == BatteryManager.BATTERY_STATUS_FULL) {
+                    // Solid green when full
                     mBatteryLight.setColor(mBatteryFullARGB);
                 } else {
                     if (isHvdcpPresent()) {
